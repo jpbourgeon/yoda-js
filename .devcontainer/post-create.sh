4d389b255c527a3acea1dad3eab81c08a2d22125
Jean-Philippe BOURGEON <3356641+jpbourgeon@users.noreply.github.com>
2021-09-12 00:01:42 +0200

    chore: vscode dev container (#1)

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+## Github CLI v1.12.1
+cd /home/node
+wget -nc https://github.com/cli/cli/releases/download/v1.12.1/gh_1.12.1_linux_amd64.deb
+dpkg -i gh_1.12.1_linux_amd64.deb
+
+### npm, pnpm
+npm install -g npm pnpm
+
+### npm-check, rush, heft
+pnpm install --global npm-check @microsoft/rush @rushstack/heft
