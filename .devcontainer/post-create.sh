830561de9cf4fb6540e2bcd47a07a75a5cbd6274
jpbourgeon <3356641+jpbourgeon@users.noreply.github.com>
2021-09-13 13:28:43 +0000

    chore: adjust devcontainer config

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+## Update the image
+apt-get update && apt-get upgrade
+
 ## Github CLI v2.0.0
 cd /home/node
 wget -nc https://github.com/cli/cli/releases/download/v2.0.0/gh_2.0.0_linux_amd64.deb
 dpkg -i gh_2.0.0_linux_amd64.deb
 
-### npm, pnpm
-npm install -g npm pnpm
+### pnpm
+npm install -g npm@7.23.0
+
+### pnpm
+npm install -g pnpm@6.7.1
 
 ### npm-check, rush, heft
-pnpm install --global npm-check @microsoft/rush @rushstack/heft
+pnpm install --global npm-check @microsoft/rush@5.53.0 @rushstack/heft@0.37.3
