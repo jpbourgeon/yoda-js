06dc4f47ea971e02ede6fd90a271b8c6c10b1246
Jean-Philippe BOURGEON <3356641+jpbourgeon@users.noreply.github.com>
2021-09-16 18:50:16 +0200

    chore(devops): add prettier (#5)
    <https://rushjs.io/pages/maintainer/enabling_prettier/>

--- apps/gh-pages/src/pages/index.js
+++ apps/gh-pages/src/pages/index.js
@@ -11,41 +11,34 @@ const FeatureList = [
     image: '/img/rts.svg',
     description: (
       <>
-        Yoda stores your data in a local instance that works offline on the client, and can replicate to and from other instances.
+        Yoda stores your data in a local instance that works offline on the client, and can replicate to and from other
+        instances.
       </>
-    ),
+    )
   },
   {
     title: 'Runs everywhere',
     image: '/img/pwa.svg',
     description: (
       <>
-        Yoda's offers different implementations that behave the same on a single instance server, in a serverless context or in the browser.
+        Yoda's offers different implementations that behave the same on a single instance server, in a serverless
+        context or in the browser.
       </>
-    ),
+    )
   },
   {
     title: 'Developer friendly',
     image: '/img/dev.svg',
-    description: (
-      <>
-        Yoda's API is meant to be familiar and consistent yet powerful and extensible.
-      </>
-    ),
-  },
+    description: <>Yoda's API is meant to be familiar and consistent yet powerful and extensible.</>
+  }
 ];
 
-function Feature({title, image, description}) {
+function Feature({ title, image, description }) {
   return (
     <div className={clsx('col col--4')}>
-      <div className="text--center">
-      </div>
+      <div className="text--center"></div>
       <div className="text--center padding-horiz--md">
-        <img
-          src={image}
-          alt={title}
-          style={{height: '10em'}}
-        />
+        <img src={image} alt={title} style={{ height: '10em' }} />
         <h3>{title}</h3>
         <p>{description}</p>
       </div>
@@ -54,27 +47,21 @@ function Feature({title, image, description}) {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`${siteConfig.tagline}`}
-      description={`${siteConfig.tagline}`}>
+    <Layout title={`${siteConfig.tagline}`} description={`${siteConfig.tagline}`}>
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">
-            <img
-              src="/img/logo_white.svg"
-              alt="yoda-js white logo"
-              style={{height: '3em'}}
-            />
+            <img src="/img/logo_white.svg" alt="yoda-js white logo" style={{ height: '3em' }} />
             <br />
             {siteConfig.title}
           </h1>
-          <p className="hero__subtitle"><strong>Y</strong>our <strong>O</strong>ffline-first <strong>DA</strong>tastore</p>
+          <p className="hero__subtitle">
+            <strong>Y</strong>our <strong>O</strong>ffline-first <strong>DA</strong>tastore
+          </p>
           <div className={styles.buttons}>
-            <Link
-              className="button button--secondary button--lg"
-              to="/docs">
+            <Link className="button button--secondary button--lg" to="/docs">
               Read the docs
             </Link>
           </div>
