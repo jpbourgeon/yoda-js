d85ba0eb7b80c7975d6dd8a341560005c6120e8e
jpbourgeon <3356641+jpbourgeon@users.noreply.github.com>
2021-09-29 07:05:42 +0000

    code refactoring (simplification), plugin options typing

--- plugins/event-emitter/src/index.ts
+++ plugins/event-emitter/src/index.ts
@@ -1,40 +1,23 @@
 import { Base } from 'javascript-plugin-architecture-with-typescript-definitions';
 import Emittery from 'emittery';
-import { ILooseBase, ILooseEventEmitter, IEventEmitter } from './types';
-
-// Use index signature to type the eventEmitter while it is under construction loosely while hooking Emittery's methods
-// <https://basarat.gitbook.io/typescript/type-system/index-signatures#typescript-index-signature>
-// It is definitely typed when the plugin returns
 
 export function eventEmitterPlugin(
-  base: Base,
+  _base: Base,
   options: Base.Options
 ): {
-  eventEmitter: IEventEmitter;
+  eventEmitter: Emittery;
 } {
-  const methodNames = Object.getOwnPropertyNames(Emittery.prototype).filter((v) => v !== 'constructor');
-
-  const eventEmitter: ILooseEventEmitter = {
-    _emittery: new Emittery({
+  return {
+    eventEmitter: new Emittery({
       debug: {
-        name: 'yoda',
-        ...options.eventEmitterDebug
+        name: options.eventEmitterDebug?.name || 'Event emitter',
+        enabled: options.eventEmitterDebug?.enabled,
+        logger: options.eventEmitterDebug?.logger
       }
     })
   };
-
-  const emitteryMethodCaller = (base: ILooseBase, methodName: string) =>
-    function (...args: unknown[]): unknown {
-      return base['eventEmitter']['_emittery'][methodName](...args);
-    };
-
-  for (const methodName of methodNames) {
-    eventEmitter[methodName] = emitteryMethodCaller(base as ILooseBase, methodName);
-  }
-
-  return { eventEmitter: eventEmitter as IEventEmitter };
 }
 
-export function withEventEmitter<T>(base: T, options: Base.Options): T & { eventEmitter: IEventEmitter } {
+export function withEventEmitterPlugin<T>(base: T, options: Base.Options): T & { eventEmitter: Emittery } {
   return Object.assign(base, eventEmitterPlugin(base as unknown as Base, options));
 }
--- plugins/event-emitter/tests/index.test.ts
+++ plugins/event-emitter/tests/index.test.ts
@@ -1,21 +1,35 @@
 import { jest } from '@jest/globals';
 import { Base } from 'javascript-plugin-architecture-with-typescript-definitions';
-import { eventEmitterPlugin, withEventEmitter } from '../src/';
+import { eventEmitterPlugin, withEventEmitterPlugin } from '../src/index';
 import Emittery from 'emittery';
-// import { IEventEmitter } from '../src/types';
 
 describe('The event emitter plugin', () => {
-  it('provides all the methods of the Emittery package', () => {
+  it('adds an instance of Emittery to the base class', () => {
     const Yoda = Base.withPlugins([eventEmitterPlugin]).withDefaults({});
     const instance = new Yoda();
-    const emitteryMethods = Object.getOwnPropertyNames(Emittery.prototype).filter((v) => v !== 'constructor');
-    const eventEmitterMethods = Object.getOwnPropertyNames(instance.eventEmitter);
-    expect(eventEmitterMethods).toEqual(expect.arrayContaining(emitteryMethods));
+    expect(instance.eventEmitter).toBeInstanceOf(Emittery);
+  });
+
+  it('provides a utility function for plugin composability', () => {
+    function emittingPlugin(base: Base, options: Base.Options) {
+      const instance = withEventEmitterPlugin(base, options);
+      async function emitNumber(n: number) {
+        await instance.eventEmitter.emit('number', n);
+      }
+      const props: { storedNumber?: number; emitNumber: (n: number) => void } = {
+        emitNumber
+      };
+      return props;
+    }
+    const Yoda = Base.withPlugins([emittingPlugin]).withDefaults({});
+    const instance = new Yoda();
+    const eventEmitterMethods = Object.getOwnPropertyNames(instance);
+    expect(eventEmitterMethods).toEqual(expect.arrayContaining(['options', 'eventEmitter', 'emitNumber']));
   });
 
   it('enables cross plugins communication', async () => {
     function emittingPlugin(base: Base, options: Base.Options) {
-      const instance = withEventEmitter(base, options);
+      const instance = withEventEmitterPlugin(base, options);
       async function emitNumber(n: number) {
         await instance.eventEmitter.emit('number', n);
       }
@@ -32,7 +46,7 @@ describe('The event emitter plugin', () => {
     }
 
     function observingPlugin(base: Base, options: Base.Options) {
-      const instance = withEmittingPlugin(withEventEmitter(base, options), options);
+      const instance = withEmittingPlugin(withEventEmitterPlugin(base, options), options);
       instance.eventEmitter.on('number', (n: number) => {
         instance.storedNumber = n;
       });
