3d53a6401afd99d7329ff8b6c23c474ed3a3d9b3
jpbourgeon <3356641+jpbourgeon@users.noreply.github.com>
2021-10-01 07:10:11 +0000

    advanced events typing

--- plugins/event-emitter/tests/index.test.ts
+++ plugins/event-emitter/tests/index.test.ts
@@ -3,6 +3,25 @@ import { Base } from 'javascript-plugin-architecture-with-typescript-definitions
 import { eventEmitterPlugin, withEventEmitterPlugin } from '../src/index';
 import Emittery from 'emittery';
 
+// Test custom Event type definition
+declare module 'javascript-plugin-architecture-with-typescript-definitions' {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Base {
+    interface EventData {
+      myEvent: number;
+    }
+  }
+}
+// Test custom Event type definition extension
+declare module 'javascript-plugin-architecture-with-typescript-definitions' {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Base {
+    interface EventData {
+      myOtherEvent: undefined;
+    }
+  }
+}
+
 describe('The event emitter plugin', () => {
   it('should add an instance of Emittery to the base class', () => {
     const Yoda = Base.withPlugins([eventEmitterPlugin]).withDefaults({});
@@ -13,31 +32,32 @@ describe('The event emitter plugin', () => {
   it('should provide a utility function for plugin composability', () => {
     function emittingPlugin(base: Base, options: Base.Options) {
       const instance = withEventEmitterPlugin(base, options);
-      async function emitNumber(n: number) {
-        await instance.eventEmitter.emit('number', n);
+      async function emitEvent() {
+        await instance.eventEmitter.emit('myOtherEvent');
       }
-      const props: { storedNumber?: number; emitNumber: (n: number) => void } = {
-        emitNumber
+      const props: { emitEvent: () => void } = {
+        emitEvent
       };
       return props;
     }
     const Yoda = Base.withPlugins([emittingPlugin]).withDefaults({});
     const instance = new Yoda();
     const eventEmitterMethods = Object.getOwnPropertyNames(instance);
-    expect(eventEmitterMethods).toEqual(expect.arrayContaining(['options', 'eventEmitter', 'emitNumber']));
+    expect(eventEmitterMethods).toEqual(expect.arrayContaining(['options', 'eventEmitter', 'emitEvent']));
   });
 
   it('should enable cross plugins communication', async () => {
     function emittingPlugin(base: Base, options: Base.Options) {
       const instance = withEventEmitterPlugin(base, options);
       async function emitNumber(n: number) {
-        await instance.eventEmitter.emit('number', n);
+        await instance.eventEmitter.emit('myEvent', n);
       }
       const props: { storedNumber?: number; emitNumber: (n: number) => void } = {
         emitNumber
       };
       return props;
     }
+
     function withEmittingPlugin<T>(
       base: T,
       options: Base.Options
@@ -47,7 +67,7 @@ describe('The event emitter plugin', () => {
 
     function observingPlugin(base: Base, options: Base.Options) {
       const instance = withEmittingPlugin(withEventEmitterPlugin(base, options), options);
-      instance.eventEmitter.on('number', (n: number) => {
+      instance.eventEmitter.on('myEvent', (n: number) => {
         instance.storedNumber = n;
       });
     }
